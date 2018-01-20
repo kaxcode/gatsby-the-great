1d598a2a9638335f48a0b48f7d5bac2f6282e210
Kenia Valladarez <keniavalladarez@gmail.com>
2018-01-19 23:37:32 -0500

    Updates index page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from "react";
 
-const IndexPage = () => (
+export default () => (
   <div>
-    <h1>Hello!</h1>
+    <h1>Hi!</h1>
+    <p>
+    Bonbon jelly bear claw biscuit sugar plum bear claw lollipop. Soufflé muffin candy sugar plum gummi bears cotton candy liquorice jelly beans chupa chups. Jelly beans tart sweet brownie bonbon cookie liquorice cake pastry. Cookie jelly sesame snaps danish soufflé sesame snaps liquorice sweet roll jelly-o. Dragée dessert tiramisu gummi bears. Ice cream caramels ice cream. Cotton candy bonbon dessert sweet sweet candy canes dragée donut. Brownie gummies tootsie roll jelly beans cotton candy.
+    </p>
   </div>
-)
+);
 
-export default IndexPage
