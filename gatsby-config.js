8ce49a67e65b76f6fa5e084bb3615f825706bce8
Kenia Valladarez <keniavalladarez@gmail.com>
2018-01-19 23:37:16 -0500

    Add new plugin to configurations.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,12 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
+  ],
 };
